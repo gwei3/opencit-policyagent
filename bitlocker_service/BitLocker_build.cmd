38bf3b8fc641933b99e474c75434872b26d10154
Animesh Patel <animeshx.patel@intel.com>
2016-05-03 20:49:17 +0530

    dev-cit-3.0-windows_support: hardend the windows batch scripts

--- bitlocker_service/BitLocker_build.cmd
+++ bitlocker_service/BitLocker_build.cmd
@@ -8,21 +8,42 @@ set me=%~n0
 set pwd=%~dp0
 
 set VsDevCmd="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-echo. Building biltlocker service....
-cd %pwd%
 
-call %VsDevCmd%
-IF NOT %ERRORLEVEL% EQU 0 (
-  echo. %me%: Visual Studio Dev Env could not be set
-  EXIT /b %ERRORLEVEL%
+IF %1=="" (
+  call:print_help
+) ELSE IF %2=="" (
+  call:print_help
+) ELSE (
+  call:ps_service_build %2 %1
 )
+GOTO:EOF
 
-msbuild BitLocker.sln /property:Configuration=Release
-IF NOT %ERRORLEVEL% EQU 0 (
-  echo. %me%: Build Failed
-  EXIT /b %ERRORLEVEL%
-)
+:ps_service_build
+  echo. Building biltlocker service....
+  cd %pwd%
 
-copy BitLocker\bin\Release\BitLocker.exe ..\policyagent\src\main\application\bin /y
+  call %VsDevCmd%
+  IF NOT %ERRORLEVEL% EQU 0 (
+    echo. %me%: Visual Studio Dev Env could not be set
+    EXIT /b %ERRORLEVEL%
+  )
+
+  msbuild BitLocker.sln /property:Configuration=%1;Platform=%2
+  IF NOT %ERRORLEVEL% EQU 0 (
+    echo. %me%: Build Failed
+    EXIT /b %ERRORLEVEL%
+  )
+
+  copy "BitLocker\bin\%2\%1\BitLocker.exe" ..\policyagent\src\main\application\bin /y
+  IF NOT %ERRORLEVEL% EQU 0 (
+    echo. "%me%: BitLocker.exe could not be copied to destination"
+    EXIT /b %ERRORLEVEL%
+  )
+  EXIT /b 0
+GOTO:EOF
+
+:print_help
+  echo. "Usage: $0 Platform Configuration"
+GOTO:EOF
 
 endlocal
--- policyagent_windows_build.cmd
+++ policyagent_windows_build.cmd
@@ -3,20 +3,72 @@ setlocal enabledelayedexpansion
 
 set me=%~n0
 set pwd=%~dp0
+set "pa_home=%pwd%"
 
-set /p makensis=Enter the NSIS binary path : 
-echo. Creating policyagent installer....
-
-call .\bitlocker-service\bitlocker_build.cmd
-IF NOT %ERRORLEVEL% EQU 0 (
-  echo. %me%: bitlocker service build failed
-  EXIT /b %ERRORLEVEL%
+IF "%NSIS_HOME%"=="" (
+  set "makensis=C:\Program Files (x86)\NSIS\makensis.exe"
+) ELSE (
+  set "makensis=%NSIS_HOME%\makensis.exe"
 )
 
-call %makensis% .\policyagent\src\main\application\policyagentinstallscript.nsi
-IF NOT %ERRORLEVEL% EQU 0 (
-  echo. %me%: policyagent install failed
-  EXIT /b %ERRORLEVEL%
+IF %1=="" (
+  call:print_help
+) ELSE IF %2=="" (
+  call:print_help
+) ELSE (
+  call:pa_installer %1 %2
 )
+GOTO:EOF
+
+:pa_installer
+  echo. Creating policyagent installer....
+  cd %pa_home%
+  cd
+  call "%pa_home%\bitlocker_service\BitLocker_build.cmd" %1 %2
+  IF NOT %ERRORLEVEL% EQU 0 (
+    echo. %me%: bitlocker service build failed
+    call:ExitBatch
+    REM EXIT /b %ERRORLEVEL%
+  )
+
+  REM call %makensis% .\policyagent\src\main\application\policyagentinstallscript.nsi
+  cd %pa_home%
+  cd
+  IF EXIST "%makensis%" (
+    echo. "%makensis% exists"
+    call "%makensis%" .\policyagent\src\main\application\policyagentinstallscript.nsi
+  ) ELSE (
+    echo. "Neither makensis.exe found at default location nor environment variable pointing to NSIS_HOME exist."
+    echo. "If NSIS not installed please install it and add NSIS_HOME environment variable in system variables"
+    call:ExitBatch
+    REM EXIT /b 1
+  )
+  IF NOT %ERRORLEVEL% EQU 0 (
+    echo. %me%: policyagent install failed
+    call:ExitBatch
+    REM EXIT /b %ERRORLEVEL%
+  )
+GOTO:EOF
+
+:print_help
+  echo. "Usage: $0 Platform Configuration"
+GOTO:EOF
+
+:ExitBatch - Cleanly exit batch processing, regardless how many CALLs
+if not exist "%temp%\ExitBatchYes.txt" call :buildYes
+call :CtrlC <"%temp%\ExitBatchYes.txt" 1>nul 2>&1
+:CtrlC
+cmd /c exit -1073741510
+
+:buildYes - Establish a Yes file for the language used by the OS
+pushd "%temp%"
+set "yes="
+copy nul ExitBatchYes.txt >nul
+for /f "delims=(/ tokens=2" %%Y in (
+  '"copy /-y nul ExitBatchYes.txt <nul"'
+) do if not defined yes set "yes=%%Y"
+echo %yes%>ExitBatchYes.txt
+popd
+exit /b
 
 endlocal
